f3675e6eb7db7f9152174618cac87aa20657b24b
juan <47119916+jhomra21@users.noreply.github.com>
2026-08-20 00:21:19 -0500

    bundle standalone Mesurer agent injector

---
+++ packages/mesurer/vite.inject.config.ts
@@ -0,0 +1,22 @@
+import solid from "@solidjs/vite-plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [solid()],
+  build: {
+    target: "esnext",
+    outDir: "dist",
+    emptyOutDir: false,
+    lib: {
+      entry: "src/inject.ts",
+      formats: ["es"],
+      fileName: "inject",
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
+});
