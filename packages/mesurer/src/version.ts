f0d495e246d2ac274f11c45be77afc4cf230ed6e
juan <47119916+jhomra21@users.noreply.github.com>
2026-08-28 21:40:26 -0500

    fix package version bundling

--- packages/mesurer/src/version.ts
+++ packages/mesurer/src/version.ts
@@ -1,4 +1,4 @@
-import packageMetadata from "../package.json";
+import { version } from "../package.json";
 
 /** Version of the public mesurer-solid package that produced this bundle. */
-export const MESURER_VERSION = packageMetadata.version;
+export const MESURER_VERSION = version;
