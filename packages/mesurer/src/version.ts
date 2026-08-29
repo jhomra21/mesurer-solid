b560e68f9fa735b3b0cbef8cc537f41787b4075f
juan <47119916+jhomra21@users.noreply.github.com>
2026-08-28 21:36:41 -0500

    fix plugin settings reset and release metadata

---
+++ packages/mesurer/src/version.ts
@@ -0,0 +1,4 @@
+import packageMetadata from "../package.json";
+
+/** Version of the public mesurer-solid package that produced this bundle. */
+export const MESURER_VERSION = packageMetadata.version;
