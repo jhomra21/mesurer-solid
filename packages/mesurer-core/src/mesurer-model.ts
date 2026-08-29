405d4825e8d308c5bd01c7a5f9d0c5927b9b144f
juan <47119916+jhomra21@users.noreply.github.com>
2026-08-29 13:07:14 -0500

    refactor: add canonical Mesurer model exports

---
+++ packages/mesurer-core/src/mesurer-model.ts
@@ -0,0 +1,5 @@
+export {
+  DEFAULT_MEASURER_SETTINGS as DEFAULT_MESURER_SETTINGS,
+  createMeasurerModelCore as createMesurerModelCore,
+} from "./model";
+export type { MeasurerCoreModel as MesurerCoreModel } from "./model";
