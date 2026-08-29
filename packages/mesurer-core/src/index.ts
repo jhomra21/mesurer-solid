8f671dd9b4c1ef66ab50591f6f174e68a7229430
juan <47119916+jhomra21@users.noreply.github.com>
2026-08-29 14:47:49 -0500

    refactor: export canonical core model directly

--- packages/mesurer-core/src/index.ts
+++ packages/mesurer-core/src/index.ts
@@ -2,7 +2,6 @@ export * from "./context";
 export * from "./domain";
 export * from "./events";
 export * from "./model";
-export * from "./mesurer-model";
 export * from "./plugins";
 export * from "./runtime";
 export * from "./store";
