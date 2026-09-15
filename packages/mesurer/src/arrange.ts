ba12ed59e24bffa13115f0514e981292d259051d
juan <47119916+jhomra21@users.noreply.github.com>
2026-09-15 10:52:53 -0500

    refactor: rename public arrange plugin factory

--- packages/mesurer/src/arrange.ts
+++ packages/mesurer/src/arrange.ts
@@ -105,7 +105,7 @@ export type MesurerArrangeService = {
   clear(): Promise<void>;
 };
 
-export const arrangePlugin = (): MesurerPlugin => ({
+export const arrange = (): MesurerPlugin => ({
   ...rendererArrangePlugin(),
   version: MESURER_VERSION,
 });
