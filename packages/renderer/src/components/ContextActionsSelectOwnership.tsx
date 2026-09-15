eaa28b8407c0ed7741ff17182232c2aa62a4e13b
juan <47119916+jhomra21@users.noreply.github.com>
2026-09-15 01:33:24 -0500

    feat: coordinate Context drafts across Select owners

---
+++ packages/renderer/src/components/ContextActionsSelectOwnership.tsx
@@ -0,0 +1,32 @@
+import { onCleanup } from "solid-js";
+import { subscribeMesurerSelectGestureStart } from "../runtime/select-gesture-channel";
+import {
+  ContextActions,
+  type ContextActionsController,
+  type ContextActionsProps,
+} from "./ContextActions";
+
+export type ContextActionsSelectOwnershipProps = ContextActionsProps & {
+  ownerWindow: Window;
+};
+
+export function ContextActionsSelectOwnership(props: ContextActionsSelectOwnershipProps) {
+  let controller: ContextActionsController | null = null;
+  const unsubscribe = subscribeMesurerSelectGestureStart(props.ownerWindow, () => {
+    controller?.abandonNoteComposer();
+  });
+  onCleanup(unsubscribe);
+
+  return (
+    <ContextActions
+      runtime={props.runtime}
+      onCopy={props.onCopy}
+      initialTriggerFallback={props.initialTriggerFallback}
+      coordinateSpace={props.coordinateSpace}
+      onController={(value) => {
+        controller = value;
+        props.onController?.(value);
+      }}
+    />
+  );
+}
